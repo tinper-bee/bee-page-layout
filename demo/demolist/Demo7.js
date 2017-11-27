918393d2f7f823dc966189462c629f0705fe30c2
ahua52 <1468492018@qq.com>
2017-11-27 10:10:41 +0800

    demo7 例子不完整

--- demo/demolist/Demo7.js
+++ demo/demolist/Demo7.js
@@ -53,31 +53,59 @@ class Demo7 extends Component {
     render() {
         return (
             <div id="demo7">
-                <Menu className="sub-inline" onClick={this.handleClick.bind(this)} style={{ width: 70 }} mode="vertical">
-                    <SubMenu  key="sub1" title={<span className="first-menu"><Icon type="uf-pc-2" /><span>组织</span></span>}>
-                        <Menu.Item key="1">选项 1</Menu.Item>
-                        <Menu.Item key="2">选项 2</Menu.Item>
-                        <Menu.Item key="3">选项 3</Menu.Item>
-                        <Menu.Item key="4">选项 4</Menu.Item>
-                        <Menu.Item key="11">选项 11</Menu.Item>
-                        <Menu.Item key="12">选项 12</Menu.Item>
-                        <Menu.Item key="13">选项 13</Menu.Item>
-                        <Menu.Item key="14">选项 14</Menu.Item>
-                    </SubMenu>
-                    <SubMenu key="sub2" title={<span className="first-menu"><Icon type="uf-pc-2" /><span>组织</span></span>}>
-                        <Menu.Item key="5">选项 5</Menu.Item>
-                        <Menu.Item key="6">选项 6</Menu.Item>
-                        <Menu.Item key="15">选项 15</Menu.Item>
-                        <Menu.Item key="16">选项 16</Menu.Item>
-                    </SubMenu>
-                    <SubMenu key="sub4" title={<span className="first-menu"><Icon type="uf-pc-2" /><span>组织</span></span>}>
-                        <Menu.Item key="9">选项 9</Menu.Item>
-                        <Menu.Item key="19">选项 19</Menu.Item>
-                        <Menu.Item key="10">选项 10</Menu.Item>
-                        <Menu.Item key="11">选项 11</Menu.Item>
-                        <Menu.Item key="12">选项 12</Menu.Item>
-                    </SubMenu>
-                </Menu>
+                <Navbar fluid expanded={this.state.expanded} >
+                   <div className="toggle-show" onClick={this.onToggle}>
+                        <Icon type="uf-align-justify" />
+                   </div>
+                   <Nav pullRight>
+                        <NavItem eventKey={3} className="icon-nav">
+                            <a href="#"> <Icon type="uf uf-qm-c"></Icon> 帮助 </a>
+                            <a className="" href="#"> 
+                                <img alt="@ahua52" className="avatar float-left mr-1" src="https://avatars2.githubusercontent.com/u/19704104?v=4&amp;s=40" height="20" width="20" />
+                                刘认华 
+                            </a>
+                        </NavItem>
+                    </Nav>
+                </Navbar>
+                <SideContainer expanded={this.state.expanded}>
+                    <Menu className="sub-inline" onClick={this.handleClick.bind(this)} style={{ width: 70 }} mode="vertical">
+                        <SubMenu  key="sub1" title={<span className="first-menu"><Icon type="uf-pc-2" /><span>组织</span></span>}>
+                            <Menu.Item key="1">选项 1</Menu.Item>
+                            <Menu.Item key="2">选项 2</Menu.Item>
+                            <Menu.Item key="3">选项 3</Menu.Item>
+                            <Menu.Item key="4">选项 4</Menu.Item>
+                            <Menu.Item key="11">选项 11</Menu.Item>
+                            <Menu.Item key="12">选项 12</Menu.Item>
+                            <Menu.Item key="13">选项 13</Menu.Item>
+                            <Menu.Item key="14">选项 14</Menu.Item>
+                        </SubMenu>
+                        <SubMenu key="sub2" title={<span className="first-menu"><Icon type="uf-pc-2" /><span>组织</span></span>}>
+                            <Menu.Item key="5">选项 5</Menu.Item>
+                            <Menu.Item key="6">选项 6</Menu.Item>
+                            <Menu.Item key="15">选项 15</Menu.Item>
+                            <Menu.Item key="16">选项 16</Menu.Item>
+                        </SubMenu>
+                        <SubMenu key="sub4" title={<span className="first-menu"><Icon type="uf-pc-2" /><span>组织</span></span>}>
+                            <Menu.Item key="9">选项 9</Menu.Item>
+                            <Menu.Item key="19">选项 19</Menu.Item>
+                            <Menu.Item key="10">选项 10</Menu.Item>
+                            <Menu.Item key="11">选项 11</Menu.Item>
+                            <Menu.Item key="12">选项 12</Menu.Item>
+                        </SubMenu>
+                    </Menu>
+                </SideContainer>
+                <div className={this.state.expanded?"inner-container expanded":"inner-container"}>
+                    <div className="inner-content">
+                        <Menu mode="inline" className="wrapper-menu" onClick={this.handleClick} openKeys={this.state.openKeys} selectedKeys={[this.state.current]} className="inner-menu" style={{ width: 240 }}>
+                            <Menu.Item key="选项 1">选项 1</Menu.Item>
+                            <Menu.Item key="选项 2">选项 2</Menu.Item>
+                            <Menu.Item key="选项 3">选项 3</Menu.Item>
+                            <Menu.Item key="选项 4">选项 4</Menu.Item>
+                            
+                        </Menu>
+                    </div>
+                    
+                </div>
             </div>
         )
     }
